924bdf1019c21878dec2f02d406d26baa814c98a
Vitor Renan <113404754+Vitao7@users.noreply.github.com>
2023-01-16 10:59:44 -0300

    Package teste

---
+++ src/teste/Main.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class Main {
+    public static void main(String[] args) {
+        //FOR
+
+        var num  = 1;
+
+        for(int j = 0; j < num; j++){
+            System.out.println(num);
+        }
+    }
+}
